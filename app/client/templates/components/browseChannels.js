0a0b36500bbd10040a07987fdc37dd2a8bc88170
HueyDuck <hueyhex@protonmail.com>
2018-03-17 21:15:54 -0400

    changes

--- app/client/templates/components/browseChannels.js
+++ app/client/templates/components/browseChannels.js
@@ -1,3 +1,22 @@
+Template['components_browseChannels'].onRendered(function(){
+    var template = this;
+	this.autorun(function(){
+        var account = web3.eth.defaultAccount;
+        TemplateVar.set(template,'account', account);
+        Subscriptions.getChannelCount(function(err, result){
+            console.log('count', result);
+        });
+        Subscriptions.getAllChannels(function(err, result){
+            console.log('eee', result);
+        });
+    });
+    
+});
+
+Template['components_browseChannels'].events({
+
+});
+
 
 Template['components_browseChannels'].helpers({
 	
